f4bcb29ce7d5f8cd8468bd3e6db0bbab51a51379
Faique Ahamad <100091756+Faiq0602@users.noreply.github.com>
2024-07-25 19:54:45 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int binarySearch(int arr[], int low , int high , int target){
     while (low <=  high){
-        int mid = low + (high - low) / 2;
+        int mid = low + (high - low) / 2; // Finding mid 
 
-        if (arr[mid] == target){
+        if (arr[mid] == target){ // If the middle element is the target
             return mid;
         }
-        else if ( arr[mid] < target){
+        else if ( arr[mid] < target){ //If the middle elements value is less than target
             low = mid + 1;
         }
         else {
-            high = mid - 1;
+            high = mid - 1; //If the middle elements value is greater than target
         }
     }
-    return -1;
+    return -1; // No element found
 }
 
 int main(){
-    int arr[] = {2,3,4,10,40};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int target = 10;
+    int arr[] = {2,3,4,10,40}; //Declaring an array (The input can be dynamic if needed)
+    int n = sizeof(arr)/sizeof(arr[0]); // Finding the size of the array
+    int target = 10; // The element to find
     int result = binarySearch(arr,0,n-1,target);
 
     if (result != -1){
